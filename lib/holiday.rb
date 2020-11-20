761d6cff42411f72311bfbf904b8fe7103a9e865
Ben Altman <altmanguitar@gmail.com>
2020-11-20 19:23:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -87,6 +87,6 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holidays.each do |season, holiday|
   binding.pry
-    holiday.collect {|name, supply| if supply.include? ("BBQ")}
+    holiday.collect |name, supply| if supply.include? ("BBQ")
   end
 end
